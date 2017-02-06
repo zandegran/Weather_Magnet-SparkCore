bbdcccfa87c1c38d78a9d55640bf2e7f894a8e23
zandegran <zandegran@gmail.com>
2017-02-06 15:37:27 +0100

    Format Change

---
+++ README.md
@@ -0,0 +1,34 @@
+Weather Magnet Spark-Core
+=========================
+Weather magnet using Particle(spark) Core
+and 4 digit 7 Segment display
+
+Building Tutorial: coming Soon
+#### Digit Configuration
+![Digit Config](http://www.learningaboutelectronics.com/images/4-digit-7-segment-LED-display.png)
+
+Image source: [www.learningaboutelectronics.com](http://www.learningaboutelectronics.com/Articles/4-digit-7-segment-LED-circuit-with-an-arduino.php) 
+
+1 		- 	Shows minus in case of negative weather
+2 & 3 	- 	Shows the temperature
+4		- 	Shows the weather status (see status codes)
+#### Status Codes
+* `U`	-	Unknown
+* `t`	-	Thunders
+* `G`	-	Good or Clear sky
+* `c`	-	Partly 
+* `C`	-	Cloudy
+* `d`	-	Drizzle
+* `r`	-	Rain
+* `S`	-	Snow
+* `F`	-	Fog or Mist
+* `E`	-	Extreme
+
+#### How to Build
+##### Using Online IDE
+* Copy the content of `weather.app` in a new project
+* Add the library `elapsedMillis` from libraries
+* Verify and Flash
+
+##### Manual
+
--- weather/application.cpp
+++ weather/application.cpp
@@ -1,31 +1,33 @@
-////http://api.openweathermap.org/data/2.5/weather?q=stockholm,SE&units=metric&appid = <app-id here>
+////http://api.openweathermap.org/data/2.5/weather?q=stockholm,SE&units=metric&appid=<app-id here>
 ////"particle webhook create webhook.json " Create the webhook
+#include "application.h"
 #include <math.h>
 int led = D7; // This one is the built-in tiny one to the right of the USB jack
 
-//ABCDEFG,dp is the order of the bits
+//ABCDEFG,dp
 const int numeral[10]= {
-B01100000, //1
-B11011010, //2
-B11110010, //3
-B01100110, //4
-B10110110, //5
-B00111110, //6
-B11100000, //7
-B11111110, //8
-B11100110, //9
+    0b11111100, //0
+    0b01100000, //1
+    0b11011010, //2
+    0b11110010, //3
+    0b01100110, //4
+    0b10110110, //5
+    0b10111110, //6
+    0b11100000, //7
+    0b11111110, //8
+    0b11110110, //9
 };
-const int scode[10]= {
-B11111100, //0
-B01100000, //1
-B11011010, //2
-B11110010, //3
-B01100110, //4
-B10110110, //5
-B00111110, //6
-B11100000, //7
-B11111110, //8
-B11100110, //9
+const int statusCode[10]= {
+    0b01111100, //U
+    0b00011110, //t
+    0b10111100, //G
+    0b00011010, //c
+    0b10011100, //C
+    0b01111010, //d
+    0b00001010, //r
+    0b10110110, //S
+    0b10001110, //F
+    0b10011110, //E
 };
 
 void displayWeather(bool minus, int temp, int status) {
@@ -35,40 +37,40 @@ void displayWeather(bool minus, int temp, int status) {
 }
 
 void processValues(const char *temperature, const char *id) {
-  Serial.println(temperature);
-  Serial.println(id);
-  //double ftemperature = strtod(temperature,NULL);
-  int intemperature =(int)(roundf(strtod(temperature,NULL)));
-  int intid = atoi(id);
-  int status=0;  //unknown U
-  if((int)(intid/100)==2)
-  {
-    status=1; //thunders t
-  }else if (intid==800)
-  {
-    status=2;  //Clear Sky G G for good
-  }else if (intid==801|| intid==802)
-  {
-    status=3;  //Light Clouds c
-  }else if (intid==804|| intid==803)
-  {
-    status=4;   //Clouds C <Caps>
-  }else if ((int)(intid/100)==3)
-  {
-    status=5;   //Drizzle d
-  }else if ((int)(intid/100)==5)
-  {
-    status=6;   //Rain r
-  }else if ((int)(intid/100)==6)
-  {
-    status=7;   //Snow s
-  }else if ((int)(intid/100)==7)
-  {
-    status=8;   //Fog F
-  }else if (intid>=900 && intid <910)
-  {
-    status=9;   //Extreme E
-  }
+    Serial.println(temperature);
+    Serial.println(id);
+    //double ftemperature = strtod(temperature,NULL);
+    int intemperature =(int)(roundf(strtod(temperature,NULL))); // Round the temperature
+    int intid = atoi(id);
+    int status=0;  //unknown 
+    if((int)(intid/100) == 2)
+    {
+        status = 1; //thunders t
+    }else if (intid == 800)
+    {
+        status = 2;  //Clear Sky/Good G 
+    }else if (intid == 801 || intid == 802)
+    {
+        status = 3;  //Light Clouds c
+    }else if (intid == 804 || intid == 803)
+    {
+        status = 4;   //Clouds C
+    }else if ((int)(intid / 100)  == 3)
+    {
+        status = 5;   //Drizzle d
+    }else if ((int)(intid / 100) == 5)
+    {
+        status = 6;   //Rain r
+    }else if ((int)(intid / 100) == 6)
+    {
+        status = 7;   //Snow s
+    }else if ((int)(intid / 100) == 7)
+    {
+        status = 8;   //Fog F
+    }else if (intid >= 900 && intid < 910)
+    {
+        status = 9;   //Extreme E
+    }
 
   displayWeather(intemperature<0, intemperature<0?intemperature*-1:intemperature, status);
 }
@@ -107,20 +109,20 @@ digitalWrite(led, LOW);
 
 }
 void setup() {
-pinMode(led, OUTPUT);
-Serial.begin(9600);
-Serial.print("Started: ");
-// Subscribe to the webhook response event
-Particle.subscribe("hook-response/weather_hook", processWeather , MY_DEVICES);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  Serial.print("Started: ");
+  // Subscribe to the webhook response event
+  Particle.subscribe("hook-response/weather_hook", processWeather , MY_DEVICES);
 }
 
 
 void loop() {
-// Get some data
-String data = String(10);
-digitalWrite(led, HIGH);
-// Trigger the webhook
-Particle.publish("weather_hook", data, PRIVATE);
-// Wait 60 seconds
-delay(30000);
+  // Get some data
+  String data = String(10);
+  digitalWrite(led, HIGH);
+  // Trigger the webhook
+  Particle.publish("weather_hook", data, PRIVATE);
+  // Wait 60 seconds
+  delay(60000);
 }
